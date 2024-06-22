57944503483f08964668d9553f8f5d66ffea897a
Devendra1011 <devendrabhatta63@gmail.com>
2024-06-22 14:22:52 +0545

    Initial commit

---
+++ app/src/main/java/com/example/tablayout/MyPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.tablayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+
+    private int numOfTabs;
+
+    public MyPagerAdapter(@NonNull FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs = numOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new ChatFragment();
+            case 2:
+                return new StatusFragment();
+            case 3:
+                return new CallFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
